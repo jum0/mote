119d552ab4b4f09f36208d3b4bed844f6a4a2e4e
jum0 <wnsah052@naver.com>
2022-08-07 03:05:08 +0900

    feat: Google Analytics 연동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,51 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import Layout from 'components/_common/components/Layout';
 import theme from '../theme';
+import * as ga from '../../lib/google-analytics';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider theme={theme}>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </ChakraProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageView(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+      `}
+      </Script>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  );
+};
 
 export default MyApp;
