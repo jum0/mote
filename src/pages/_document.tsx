d6e6374402c63105394a206ad7d4c4455d3e7444
jum0 <wnsah052@naver.com>
2022-08-18 01:49:44 +0900

    feat: open graph 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,16 @@ export default class Document extends NextDocument {
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
+          <meta property="og:title" content="mote - 주식 계산기" />
+          <meta property="og:url" content="https://mote.vercel.app/" />
+          <meta property="og:description" content="수익과 수수료를 확인해 보세요!" />
+          <meta
+            property="og:image"
+            content="https://user-images.githubusercontent.com/40762111/185186620-1b599404-be7e-408e-8522-2d21e4b3e57f.png"
+          />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:locale" content="ko_KR" />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
