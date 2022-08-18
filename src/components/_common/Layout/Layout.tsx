21d4e29d18acc7945590066fb199a4c98703a9a7
jum0 <wnsah052@naver.com>
2022-08-19 03:35:58 +0900

    refactor: 스타일 파일 통합

--- src/components/_common/Layout/Layout.tsx
+++ src/components/_common/Layout/Layout.tsx
@@ -1,10 +1,20 @@
-import { Box, Container, GridItem } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import { Box, Container, Grid as BaseGrid, GridItem } from '@chakra-ui/react';
 
 import BottomNavigation from '../BottomNavigation';
 
 import { COMMON_BOTTOM_NAVIGATION_HEIGHT } from 'constants/_common';
 
-import * as Styled from './Layout.styled';
+const Grid = styled(BaseGrid)`
+  height: 100vh;
+
+  /* iOS only */
+  @media not all and (min-resolution: 0.001dpcm) {
+    @supports (-webkit-appearance: none) {
+      height: -webkit-fill-available;
+    }
+  }
+`;
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -13,14 +23,14 @@ type LayoutProps = {
 const Layout = ({ children }: LayoutProps) => (
   <Box backgroundColor="gray.100">
     <Container maxWidth="container.md" padding="0">
-      <Styled.Grid templateRows={`auto ${COMMON_BOTTOM_NAVIGATION_HEIGHT}px`}>
+      <Grid templateRows={`auto ${COMMON_BOTTOM_NAVIGATION_HEIGHT}px`}>
         <GridItem overflow="auto">
           <Box height="100%">{children}</Box>
         </GridItem>
         <GridItem>
           <BottomNavigation />
         </GridItem>
-      </Styled.Grid>
+      </Grid>
     </Container>
   </Box>
 );
--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -2,15 +2,14 @@ import { useCallback, useState } from 'react';
 
 import { useForm } from 'react-hook-form';
 
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import { Box, Button, Flex, Icon, Text } from '@chakra-ui/react';
 import { AiOutlineMail } from 'react-icons/ai';
 
 import type { SpreadSheets } from 'domains/spreadsheets/spreadsheets.type';
 import * as Mapper from 'domains/spreadsheets/spreadsheets.mapper';
 
 import AutoResizeTextarea from 'components/feedback/AutoResizeTextarea';
-
-import * as Styled from './feedback.styled';
 
 const Feedback = () => {
   const {
@@ -63,7 +62,7 @@ const Feedback = () => {
           </Text>
         </Box>
         <Box display="flex" justifyContent="center">
-          <Styled.AnimatedIcon
+          <AnimatedIcon
             as={AiOutlineMail}
             width={{ base: '120px', sm: '200px' }}
             height={{ base: '120px', sm: '200px' }}
@@ -100,3 +99,68 @@ const Feedback = () => {
 };
 
 export default Feedback;
+
+const AnimatedIcon = styled(Icon)`
+  -webkit-animation: wobble-hor-bottom 1.6s both;
+  animation: wobble-hor-bottom 1.6s both;
+  animation-iteration-count: infinite;
+
+  @-webkit-keyframes wobble-hor-bottom {
+    0%,
+    67% {
+      -webkit-transform: translateX(0%);
+      transform: translateX(0%);
+      -webkit-transform-origin: 50% 50%;
+      transform-origin: 50% 50%;
+    }
+    10% {
+      -webkit-transform: translateX(-30px) rotate(-6deg);
+      transform: translateX(-30px) rotate(-6deg);
+    }
+    20% {
+      -webkit-transform: translateX(15px) rotate(6deg);
+      transform: translateX(15px) rotate(6deg);
+    }
+    30% {
+      -webkit-transform: translateX(-15px) rotate(-3.6deg);
+      transform: translateX(-15px) rotate(-3.6deg);
+    }
+    45% {
+      -webkit-transform: translateX(9px) rotate(2.4deg);
+      transform: translateX(9px) rotate(2.4deg);
+    }
+    50% {
+      -webkit-transform: translateX(-6px) rotate(-1.2deg);
+      transform: translateX(-6px) rotate(-1.2deg);
+    }
+  }
+  @keyframes wobble-hor-bottom {
+    0%,
+    67% {
+      -webkit-transform: translateX(0%);
+      transform: translateX(0%);
+      -webkit-transform-origin: 50% 50%;
+      transform-origin: 50% 50%;
+    }
+    10% {
+      -webkit-transform: translateX(-30px) rotate(-6deg);
+      transform: translateX(-30px) rotate(-6deg);
+    }
+    20% {
+      -webkit-transform: translateX(15px) rotate(6deg);
+      transform: translateX(15px) rotate(6deg);
+    }
+    30% {
+      -webkit-transform: translateX(-15px) rotate(-3.6deg);
+      transform: translateX(-15px) rotate(-3.6deg);
+    }
+    40% {
+      -webkit-transform: translateX(9px) rotate(2.4deg);
+      transform: translateX(9px) rotate(2.4deg);
+    }
+    50% {
+      -webkit-transform: translateX(-6px) rotate(-1.2deg);
+      transform: translateX(-6px) rotate(-1.2deg);
+    }
+  }
+`;
