0e27dccabe980a7a0d80fc457fe20a960a620c7b
jum0 <wnsah052@naver.com>
2022-08-19 02:29:11 +0900

    feat: 아이콘 애니메이션 추가

--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -6,8 +6,74 @@ import * as Mapper from 'domains/spreadsheets/spreadsheets.mapper';
 
 import AutoResizeTextarea from 'components/feedback/components/AutoResizeTextarea';
 
+import styled from '@emotion/styled';
 import { Box, Button, Flex, Icon, Text } from '@chakra-ui/react';
 import { AiOutlineMail } from 'react-icons/ai';
+
+const AnimatedIcon = styled(Icon)`
+  -webkit-animation: wobble-hor-bottom 1.6s both;
+  animation: wobble-hor-bottom 1.6s both;
+  animation-iteration-count: infinite;
+
+  @-webkit-keyframes wobble-hor-bottom {
+    0%,
+    67% {
+      -webkit-transform: translateX(0%);
+      transform: translateX(0%);
+      -webkit-transform-origin: 50% 50%;
+      transform-origin: 50% 50%;
+    }
+    10% {
+      -webkit-transform: translateX(-30px) rotate(-6deg);
+      transform: translateX(-30px) rotate(-6deg);
+    }
+    20% {
+      -webkit-transform: translateX(15px) rotate(6deg);
+      transform: translateX(15px) rotate(6deg);
+    }
+    30% {
+      -webkit-transform: translateX(-15px) rotate(-3.6deg);
+      transform: translateX(-15px) rotate(-3.6deg);
+    }
+    45% {
+      -webkit-transform: translateX(9px) rotate(2.4deg);
+      transform: translateX(9px) rotate(2.4deg);
+    }
+    50% {
+      -webkit-transform: translateX(-6px) rotate(-1.2deg);
+      transform: translateX(-6px) rotate(-1.2deg);
+    }
+  }
+  @keyframes wobble-hor-bottom {
+    0%,
+    67% {
+      -webkit-transform: translateX(0%);
+      transform: translateX(0%);
+      -webkit-transform-origin: 50% 50%;
+      transform-origin: 50% 50%;
+    }
+    10% {
+      -webkit-transform: translateX(-30px) rotate(-6deg);
+      transform: translateX(-30px) rotate(-6deg);
+    }
+    20% {
+      -webkit-transform: translateX(15px) rotate(6deg);
+      transform: translateX(15px) rotate(6deg);
+    }
+    30% {
+      -webkit-transform: translateX(-15px) rotate(-3.6deg);
+      transform: translateX(-15px) rotate(-3.6deg);
+    }
+    40% {
+      -webkit-transform: translateX(9px) rotate(2.4deg);
+      transform: translateX(9px) rotate(2.4deg);
+    }
+    50% {
+      -webkit-transform: translateX(-6px) rotate(-1.2deg);
+      transform: translateX(-6px) rotate(-1.2deg);
+    }
+  }
+`;
 
 const Feedback = () => {
   const {
@@ -60,7 +126,12 @@ const Feedback = () => {
           </Text>
         </Box>
         <Box display="flex" justifyContent="center">
-          <Icon as={AiOutlineMail} width={{ base: '120px', sm: '160px' }} height={{ base: '120px', sm: '160px' }} color="purple.400" />
+          <AnimatedIcon
+            as={AiOutlineMail}
+            width={{ base: '120px', sm: '200px' }}
+            height={{ base: '120px', sm: '200px' }}
+            color="purple.400"
+          />
         </Box>
         <Box>
           <Button width="100%" onClick={handleConfirmButtonClick}>
